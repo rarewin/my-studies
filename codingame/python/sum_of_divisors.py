806c7f8256f0db756cc1e0c534f031f88589df75
Katsuki Kobayashi <rare@tirasweel.org>
2016-06-21 00:26:48 +0900

    try Rust!

---
+++ codingame/python/sum_of_divisors.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+
+# Write an action using print
+# To debug: print("Debug messages...", file=sys.stderr)
+
+print("sum")
