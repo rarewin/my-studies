18bb1b4cfd330e6e05360fe0a84bb455cb0dfc9c
Katsuki Kobayashi <rare@tirasweel.org>
2017-05-16 00:50:16 +0900

    add edit page

--- python/bottle-test/tutorial_todo-list/todo.py
+++ python/bottle-test/tutorial_todo-list/todo.py
@@ -31,18 +31,46 @@ def new_item():
 
         new = request.GET.task.strip()
 
-        conn = sqlite3.connect('todo.db')
-        c = conn.cursor()
+        con = sqlite3.connect('todo.db')
+        c = con.cursor()
 
         c.execute("INSERT INTO todo (task, status) VALUES (?, ?)", (new, 1))
         new_id = c.lastrowid
 
-        conn.commit()
+        con.commit()
         c.close()
 
         ret.update({'created_task': 'The new task was inserted into the database, the ID is %s' % new_id})
 
     return ret
+
+@route('/edit/<no:int>', method = 'GET')
+@view('edit_task.tpl.html')
+def edit_item(no):
+
+    if request.GET.save:
+
+        edit   = request.GET.task.strip()
+        status = request.GET.status.strip()
+
+        status = 1 if status == 'open' else 0
+
+        con = sqlite3.connect('todo.db')
+        c = con.cursor()
+        c.execute("UPDATE todo SET task = ?, status = ? WHERE id LIKE ?", (edit, status, no))
+        con.commit()
+        cur_data = [edit,]
+
+    else:
+
+        con = sqlite3.connect('todo.db')
+        c = con.cursor()
+        c.execute("SELECT task FROM todo WHERE id LIKE ?", (str(no)))
+        cur_data = c.fetchone()
+
+    return {'no':  no,
+            'old': cur_data}
+
 
 if __name__ == '__main__':
     run(port = 8080, debug = True, reloader = True)
