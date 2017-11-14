bfb13b5cc31c21aae38138b5a13e7fb3e04bb9f6
Katsuki Kobayashi <rare@tirasweel.org>
2017-11-14 20:39:48 +0900

    try some vue samples

--- rust/test-trait/src/lib.rs
+++ rust/test-trait/src/lib.rs
@@ -1,7 +1,29 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+pub trait Summarizable {
+    fn summary(&self) -> String;
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summarizable for NewsArticle {
+    fn summary(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summarizable for Tweet {
+    fn summary(&self) -> String {
+        format!("{}: {}", self.username, self.content)
     }
 }
